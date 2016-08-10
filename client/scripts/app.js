ab07ab220fd32151cd57ba75172ac6ed5fe2ce0a
MikeBordelon <374mike@gmail.com>
2016-08-10 16:04:07 -0700

    Pass tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,9 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    var likeOrNot = this.get('like');
+    this.set({like: !likeOrNot});
+
   }
 
 });
@@ -15,13 +17,15 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', this.sort, this);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    console.log('runs');
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -58,7 +62,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', this.render, this);
   },
 
   events: {
@@ -66,7 +70,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +83,8 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    // this.collection.on('change', this.render, this);
+    this.collection.on('all', this.render, this);
   },
 
   render: function() {
